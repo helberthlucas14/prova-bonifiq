43966fbd6babacb18ea170ee82734ad7092e051c
Helberth Versiani <helberthlucas@gmail.com>
2025-08-15 13:38:37 -0300

    fix: alteração ciclo de vida RandomService Sigleton para Transient, implementação de verificação se o Number ja existe, melhoria de geração de numero randomico

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -1,29 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProvaPub.Models;
 using ProvaPub.Repository;
+using System.Security.Cryptography;
 
 namespace ProvaPub.Services
 {
-	public class RandomService
-	{
-		int seed;
+    public class RandomService
+    {
         TestDbContext _ctx;
-		public RandomService()
+        public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
     .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
     .Options;
-            seed = Guid.NewGuid().GetHashCode();
-
             _ctx = new TestDbContext(contextOptions);
         }
         public async Task<int> GetRandom()
-		{
-            var number =  new Random(seed).Next(100);
+        {
+            var number = GenerateRandomNumber();
+
+            while (ExistNumber(number))
+                number = GenerateRandomNumber();
+
             _ctx.Numbers.Add(new RandomNumber() { Number = number });
             _ctx.SaveChanges();
-			return number;
-		}
-
-	}
+            return number;
+        }
+        private bool ExistNumber(int number) => _ctx.Numbers.Any(x => x.Number == number);
+        private int GenerateRandomNumber()
+        {
+            byte[] bytes = new byte[4];
+            RandomNumberGenerator.Fill(bytes);
+            int value = BitConverter.ToInt32(bytes, 0) & int.MaxValue;
+            return value;
+        }
+    }
 }
